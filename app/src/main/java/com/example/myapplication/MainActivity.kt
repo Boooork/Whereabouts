151b6efdbc16676b0fef63f3a447c92c85afdd65
aiden_Johnson <prezetti@gmail.com>
2025-02-11 15:21:19 -0500

    Arbitrary save before switching a fragment to be an activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.initialize
@@ -23,7 +24,10 @@ class MainActivity : AppCompatActivity() {
        // val db = FirebaseFirestore.getInstance()
         setContentView(R.layout.fragment_signup)
 
-
+        val signUptoIn = findViewById<TextView>(R.id.signInRedirect)
+        signUptoIn.setOnClickListener {
+            //val intent = Intent(this, )
+        }
 
     }
 
