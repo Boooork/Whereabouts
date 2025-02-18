00aa0ce995493348c60b9a2b77e5f64919d47e78
aiden_Johnson <prezetti@gmail.com>
2025-02-18 17:28:08 -0500

    Arbitrary safety commit, no new features.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,11 +49,28 @@ dependencies {
 
 
     // Import the BoM for the Firebase platform
-    implementation(libs.firebase.auth)
+   // implementation(libs.firebase.auth)
+
+// Import the BoM for the Firebase platform
+    implementation(platform(libs.firebase.bom))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(libs.com.google.firebase.firebase.auth)
+
 
 
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+                     //IF ANY LINKING ERRORS OCCUR, CHECK THIS IMP.
+    //implementation(libs.kotlinx.coroutines.play.services)
+
+    //implementation(libs.kotlinx.coroutines.core)
+    //implementation(libs.kotlinx.corounties.android)
+
 }
---
+++ app/src/main/java/com/example/myapplication/ForumBoard.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityForumBoardBinding
+
+class ForumBoard : AppCompatActivity() {
+
+private lateinit var binding: ActivityForumBoardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityForumBoardBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+      val addButton = findViewById<Button>(R.id.addPostButton)
+      val backButton = findViewById<Button>(R.id.backButton)
+      val trashButton = findViewById<Button>(R.id.deletePostButton)
+
+
+
+
+    }
+
+
+
+}
